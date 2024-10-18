43e8c5e2399e16558bc06b855096e0158c7ec306
klealufide <kleal40321@ufide.ac.cr>
2024-10-18 02:00:40 -0600

    Semana 5

--- clases/semana4.js
+++ clases/semana4.js
@@ -60,3 +60,59 @@ switch (color) {
         console.log("No esta definido ese color");
         break;
 }
+
+for(let i = 0; i < 10 ; i++){
+    console.log(i)
+}
+
+let frutas = ["melon","sandia","manzana","pera"];
+
+for(let i in frutas){
+    console.log(frutas[i]);
+}
+
+console.log(frutas[0]);
+
+for(let i in frutas){
+    console.log(i);
+    if(frutas[i] == "melon"){
+        console.log("Encontre mi fruta favorita", frutas[i]);
+        break;
+    }
+}
+
+
+edad = 50;
+
+do {
+    edad++;
+    console.log(edad);
+} while(edad < 50)
+
+
+while(edad < 50){
+    edad++;
+    console.log(edad);
+}
+
+function calculadora(operador, numero1, numero2){
+    // sumar, restar, multiplica, dividir
+
+
+}
+
+console.log(calculadora("",5,6));
+/*
+let saludo = document.getElementById("saludo");
+saludo.innerHTML = "Hola Semana 5";
+console.log(saludo);
+*/
+let nuevoElemento = document.createElement("p");
+nuevoElemento.innerHTML = "<b>Este es un nuevo elemento tipo p</b>";
+document.body.append(nuevoElemento);
+
+document.getElementById("saludar").addEventListener("click",function(){
+    let saludo = document.getElementById("saludo");
+    saludo.innerHTML = "Hola Semana 5";
+    alert("Hola!!");
+})
